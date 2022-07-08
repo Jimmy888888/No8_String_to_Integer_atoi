dfb2bbb777e05e82f6b7450b595a7f379493c5cc
康任均 <kangrenjun@kangrenjundeMacBook-Air.local>
2022-07-08 22:24:03 +0800

    2022/0/08 update01 No8 go

--- No8.go
+++ No8.go
@@ -1,3 +1,42 @@
-func myAtoi(s string) int {
+package main
 
+import "unicode"
+
+func myAtoi(s string) int {
+	positive := '+'
+	pos_index := -1
+	negative := '-'
+	neg_index := -1
+	num_start := false
+	head_index := -1
+	tail_index := -1
+	whitespace := ' '
+
+	for sindex, schar := range s {
+		if schar == whitespace {
+			continue
+		} else if schar == negative {
+			neg_index = sindex
+		} else if schar == positive {
+			pos_index = sindex
+		} else if unicode.IsNumber(schar) && num_start == false { //schar is number
+			if schar == '0' && num_start == false {
+				continue
+			}
+
+			if schar != '0' && num_start == false {
+				num_start = true
+				head_index = sindex
+			}
+		} else if unicode.IsNumber(schar) == false && num_start == true { //number not continuous
+			tail_index = sindex
+			num_start = false
+		}
+	}
+
+	if head_index >= 0 {
+
+	}
+
+	return 0
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module No8_String_to_Integer_atoi
+
+go 1.18
